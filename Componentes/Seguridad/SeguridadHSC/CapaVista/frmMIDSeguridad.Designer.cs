7beb9ba567b1a8544270135d209c105c6361b63a
achacon845 <achacon845@gmail.com>
2022-04-20 12:27:58 -0600

    Contenedor MDI, con los menús (Abrir, catálogos, procesos, informes, herramientas y

---
+++ Componentes/Seguridad/SeguridadHSC/CapaVista/frmMIDSeguridad.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace CapaVistaSeguridadHSC
+{
+    partial class frmMIDSeguridad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMIDSeguridad));
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.salirToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mantenimientosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.asignacionesToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnCambioContraseña = new System.Windows.Forms.ToolStripMenuItem();
+            this.herramientasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inicioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnCerrarSesion = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salirToolStripMenuItem1,
+            this.toolStripMenuItem1,
+            this.mantenimientosToolStripMenuItem,
+            this.asignacionesToolStripMenuItem1,
+            this.btnCambioContraseña,
+            this.herramientasToolStripMenuItem,
+            this.ayudaToolStripMenuItem,
+            this.inicioToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip.TabIndex = 3;
+            this.menuStrip.Text = "MenuStrip";
+            // 
+            // salirToolStripMenuItem1
+            // 
+            this.salirToolStripMenuItem1.Name = "salirToolStripMenuItem1";
+            this.salirToolStripMenuItem1.Size = new System.Drawing.Size(54, 24);
+            this.salirToolStripMenuItem1.Text = "Abrir";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(12, 24);
+            // 
+            // mantenimientosToolStripMenuItem
+            // 
+            this.mantenimientosToolStripMenuItem.Name = "mantenimientosToolStripMenuItem";
+            this.mantenimientosToolStripMenuItem.Size = new System.Drawing.Size(93, 24);
+            this.mantenimientosToolStripMenuItem.Text = "Catalogos";
+            // 
+            // asignacionesToolStripMenuItem1
+            // 
+            this.asignacionesToolStripMenuItem1.Name = "asignacionesToolStripMenuItem1";
+            this.asignacionesToolStripMenuItem1.Size = new System.Drawing.Size(87, 24);
+            this.asignacionesToolStripMenuItem1.Text = "Procesos";
+            // 
+            // btnCambioContraseña
+            // 
+            this.btnCambioContraseña.Name = "btnCambioContraseña";
+            this.btnCambioContraseña.Size = new System.Drawing.Size(84, 24);
+            this.btnCambioContraseña.Text = "Informes";
+            this.btnCambioContraseña.Click += new System.EventHandler(this.btnCambioContraseña_Click);
+            // 
+            // herramientasToolStripMenuItem
+            // 
+            this.herramientasToolStripMenuItem.Name = "herramientasToolStripMenuItem";
+            this.herramientasToolStripMenuItem.Size = new System.Drawing.Size(117, 24);
+            this.herramientasToolStripMenuItem.Text = "Herramientas";
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(66, 24);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // inicioToolStripMenuItem
+            // 
+            this.inicioToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salirToolStripMenuItem,
+            this.btnCerrarSesion});
+            this.inicioToolStripMenuItem.Name = "inicioToolStripMenuItem";
+            this.inicioToolStripMenuItem.Size = new System.Drawing.Size(52, 24);
+            this.inicioToolStripMenuItem.Text = "Salir";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(175, 24);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // btnCerrarSesion
+            // 
+            this.btnCerrarSesion.Name = "btnCerrarSesion";
+            this.btnCerrarSesion.Size = new System.Drawing.Size(175, 24);
+            this.btnCerrarSesion.Text = "Cerrar Sesión";
+            this.btnCerrarSesion.Click += new System.EventHandler(this.btnCerrarSesion_Click);
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(688, 4);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtUsuario.TabIndex = 5;
+            this.txtUsuario.Visible = false;
+            // 
+            // frmMIDSeguridad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.menuStrip);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.IsMdiContainer = true;
+            this.Name = "frmMIDSeguridad";
+            this.Text = "Seguridad HSC";
+            this.Load += new System.EventHandler(this.frmLoginHSC_Load);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem inicioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem btnCerrarSesion;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem mantenimientosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem asignacionesToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem btnCambioContraseña;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem herramientasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+    }
+}
---
+++ Componentes/Seguridad/SeguridadHSC/CapaVista/frmMIDSeguridad.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Windows.Forms;
+using BitacoraUsuario;
+using CapaControladorSeguridadHSC;
+using static datosUsuario;
+//Forma Creada por Roberto Lopez 0901-18-4982
+namespace CapaVistaSeguridadHSC
+{
+    public partial class frmMIDSeguridad : Form
+    {
+        private ObtenerPermisos global = new ObtenerPermisos();
+
+        public frmMIDSeguridad()
+        {
+            InitializeComponent();
+            CenterToScreen();
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        CapaControladorSeguridadHSC.Controlador bitacora = new CapaControladorSeguridadHSC.Controlador();
+        //Kevin Flores 9959-18-17632
+        private void btnCerrarSesion_Click(object sender, EventArgs e)
+        {
+            frmLoginHSC form = new frmLoginHSC();
+            if (form.ShowDialog() == DialogResult.OK)
+            {
+                //Jorge González 0901-18-3920
+
+                bitacora.guardarEnBitacora(IdUsuario, "1", "0001", "Cerrar sesión");
+                txtUsuario.Text = form.usuario();
+            }
+            else { this.Close(); }
+        }
+
+
+        private void btnPerfiles_Click(object sender, EventArgs e)
+        {
+           /* frmMantenimientoPerfil form3 = new frmMantenimientoPerfil();
+            form3.MdiParent = this;
+            //Jorge González 0901-18-3920
+            bitacora.guardarEnBitacora(IdUsuario, "1", "0007", "Entrada a la Vista");
+            form3.Show();*/
+        }
+       
+        private void btnCambioContraseña_Click(object sender, EventArgs e)
+        {
+            frmCambioContraseña form3 = new frmCambioContraseña();
+            form3.MdiParent = this;
+            //Jorge González 0901-18-3920
+            bitacora.guardarEnBitacora(IdUsuario, "1", "0010", "Entrada a la Vista");
+            form3.Show();
+        }
+
+        private void btnAsiginaciónDePerfilesAUsuarios_Click(object sender, EventArgs e)
+        {
+            /*frmPerfiles form3 = new frmPerfiles();
+            form3.MdiParent = this;
+            //Jorge González 0901-18-3920
+            bitacora.guardarEnBitacora(IdUsuario, "1", "0007", "Asignación de Perfiles a Usuarios");
+            form3.Show();*/
+        }
+        
+        private void bitácoraDeLoginToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bitacora.guardarEnBitacora(IdUsuario, "1", "0011", "Entrada a la Vista");
+            frmBitacora form3 = new frmBitacora();
+            form3.MdiParent = this;
+            //Jorge González 0901-18-3920
+            form3.Show();
+        }
+        
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        
+        private void frmLoginHSC_Load(object sender, EventArgs e)
+        {
+            frmLoginHSC form = new frmLoginHSC();
+            if (form.ShowDialog() == DialogResult.OK)
+            {
+                txtUsuario.Text = form.usuario();
+                global.usuarioglobal = txtUsuario.Text;
+            }
+            else
+            {
+                this.Close();
+            }
+        }
+
+    }
+}
